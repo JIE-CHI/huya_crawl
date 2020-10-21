2908a10f4148553098c53ad3d44e214c15638983
JIE-CHI <chijie1015@gmail.com>
2020-10-21 06:13:57 +0100

    autodetect date

--- gbk.sh
+++ gbk.sh
@@ -1,1 +1,4 @@
-/usr/bin/rclone sync --update --verbose --transfers 30 --checkers 8 --contimeout 60s --local-no-check-updated  --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "/home/jiechi/workspace/github/huya_scraper/2020_10_20" "google-drive:MFen/2020_10_20"
+#!/bin/bash
+sync_dir=$(date +'%Y_%m_%d')
+echo ${sync_dir}
+watch -n 30  "rclone sync --update --verbose --transfers 30 --checkers 8 --contimeout 60s --local-no-check-updated  --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "/home/jiechi/workspace/github/huya_scraper/${sync_dir}" "google-drive:MFen/${sync_dir}""
