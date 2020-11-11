4925fe51fa1bf28e218a064470c37e08043e9a14
JIE-CHI <chijie1015@gmail.com>
2020-11-11 03:44:03 +0000

    Delete gbk.sh

--- gbk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sync_dir=$(date +'%Y_%m_%d')
-echo ${sync_dir}
-watch -n 30  "rclone sync --update --verbose --transfers 30 --checkers 8 --contimeout 60s --local-no-check-updated  --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "/home/jiechi/workspace/github/huya_scraper/${sync_dir}" "google-drive:MFen/${sync_dir}""
